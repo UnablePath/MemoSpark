03eb10a68fd32133785138a15dc79a9a933eccc2
Kekeli Kweku Biga <kekeli.biga@ashesi.edu.gh>
2025-04-12 17:59:47 +0000

    idk bro

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -15,7 +15,7 @@ export default function LoginPage() {
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
-          <Icons.logo className="mx-auto h-8 w-8" />
+          <Icons.user className="mx-auto h-8 w-8" />
           <h1 className="text-2xl font-semibold tracking-tight">Welcome back</h1>
           <p className="text-sm text-muted-foreground">
             Sign in to your account to continue
